2ebcc783c23bd240de6b1f908335b1c7c1be2ab3
Jabran Rafique <hello@jabran.me>
2015-09-30 15:53:17 +0100

    remove dev use ignores

---
+++ model/User.php
@@ -0,0 +1,12 @@
+<?php namespace Lassi\Model;
+
+/**
+ * User
+ *
+ * @author Jabran Rafique <hello@jabran.me>
+ * @license MIT License
+ */
+
+use \Illuminate\Database\Eloquent\Model;
+
+class User extends Model { }
