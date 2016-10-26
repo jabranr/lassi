1d14e66254c4ef308304462577f3d8b3942d6c6d
Jabran Rafique <hello@jabran.me>
2016-10-27 00:19:29 +0100

    add new exception

---
+++ Lassi/App/Exception/UnreadableException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lassi\App\Exception;
+
+/**
+ * Unreadable exception
+ *
+ * @author Jabran Rafique <hello@jabran.me>
+ * @license MIT License
+ */
+
+class UnreadableException extends \RuntimeException { }
